9aa0b95418a65c4df1cab2ec6b55b26616fdac0b
Wesley Felipe <wesleys.felipe@gmail.com>
2017-04-21 11:42:01 -0300

    Temporizadores

--- PThreadsPAD/Main.cpp
+++ PThreadsPAD/Main.cpp
@@ -1,6 +1,7 @@
 #include <windows.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 #include "FiltroBrilho.h"
 #include "ImageReader.h"
@@ -12,7 +13,14 @@ using namespace std;
 void executarFiltro(char* nomeArquivo,  char* nomeArquivoResultado, int fBrilho, int qtdThreads);
 
 int main(int argc, char** argv) {
+
+	printf("Iniciando programa \n");
+	clock_t tStart = clock();
+
 	executarFiltro("Imagens/baby-groot.ppm", "Imagens/baby-groot2.ppm", 30, 5);
+
+	printf("Execução encerrada.\n");
+	printf("Tempo total de execução: %.2fs\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
 }
 
 void executarFiltro(char* nomeArquivo, char* nomeArquivoResultado, int fBrilho, int qtdThreads) {
