0a2cc8cf4388a0cf6d7d528c2f1953e31ca38efe
Wesley Felipe <wesleys.felipe@gmail.com>
2017-04-21 23:18:48 -0300

    Multiplas threads

--- PThreadsPAD/Main.cpp
+++ PThreadsPAD/Main.cpp
@@ -1,47 +1,56 @@
-#include <windows.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #define HAVE_STRUCT_TIMESPEC
 #include <pthread.h>
 
-#include "FiltroBrilho.h"
-#include "ImageReader.h"
-#include "Imagem.h"
+#define N 10000
+#define N_THREADS 5
+int saldo = 100;
+pthread_mutex_t m;
 
-using namespace std;
-
-#define QTD_THREADS 5
-
-void *doSomething(void *parameter);
-void teste_threads();
-
-int main(int argc, char** argv) {
-	teste_threads();
+void * deposita(void *str) {
+	int i, a;
+	for (i = 0; i<N; i++) {
+		pthread_mutex_lock(&m);
+		a = saldo; a = a + 1; saldo = a;
+		pthread_mutex_unlock(&m);
+	}
+	return 0;
 }
 
-/* Metodo criado só para validar se Pthreads está funcionando*/
-void teste_threads() {
-	alterarBrilho();
-
-	Imagem* imagem = lerImagem("Imagens/background.ppm");
-	printf("Altura da imagem: %d\n", imagem->getHeight());
-
-	Sleep(1000);
-
-	pthread_t threads[QTD_THREADS];
-
-	for (int i = 0; i < QTD_THREADS; i++) {
-		pthread_t thread_obj;
-		pthread_create(&thread_obj, NULL, doSomething, NULL);
-		threads[i] = thread_obj;
+void * retira(void *str) {
+	int i, b;
+	for (i = 0; i<N; i++) {
+		pthread_mutex_lock(&m);
+		b = saldo; b = b - 1; saldo = b;
+		pthread_mutex_unlock(&m);
 	}
-
-	for (int i = 0; i < QTD_THREADS; i++) {
-		pthread_join(threads[i], NULL);
-	}
+	return 0;
 }
 
-void *doSomething(void *parameter) {
-	printf("Eu sou uma thread\n");
-	return (void *)0;
+int main() {
+
+	pthread_t threads_deposita[N_THREADS];
+	pthread_t threads_retira[N_THREADS];
+
+	pthread_mutex_init(&m, NULL);
+
+	for (int i = 0; i < N_THREADS; i++) {
+		pthread_t thid1, thid2;
+
+		pthread_create(&thid1, NULL, deposita, NULL);
+		threads_deposita[i] = thid1;
+
+		pthread_create(&thid2, NULL, retira, NULL);
+		threads_retira[i] = thid2;
+
+	}
+
+	for (int i = 0; i < N_THREADS; i++) {
+		pthread_join(threads_retira[i], NULL);
+		pthread_join(threads_deposita[i], NULL);
+	}
+
+	printf("SALDO ATUAL = %d\n", saldo);
+	return 0;
 }
