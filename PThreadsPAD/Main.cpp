ec32936dfea7183ca151a3ca1778fa8a52c5c302
Wesley Felipe <wesleys.felipe@gmail.com>
2017-04-21 22:39:50 -0300

    Testes aplicados

--- PThreadsPAD/Main.cpp
+++ PThreadsPAD/Main.cpp
@@ -12,15 +12,34 @@ using namespace std;
 
 void executarFiltro(char* nomeArquivo,  char* nomeArquivoResultado, int fBrilho, int qtdThreads);
 
+void executarTestesPerformance(char* nomeArquivo, int maxThreads);
+
 int main(int argc, char** argv) {
 
-	printf("Iniciando programa \n");
-	clock_t tStart = clock();
+	printf("Iniciando teste de performance. \n");
 
-	executarFiltro("Imagens/baby-groot.ppm", "Imagens/baby-groot2.ppm", 30, 5);
+	executarTestesPerformance("Imagens/baby-groot-2.ppm", 3);
 
-	printf("Execução encerrada.\n");
-	printf("Tempo total de execução: %.2fs\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
+	printf("Teste de performance finalizado. \n");
+
+}
+
+void executarTestesPerformance(char* nomeArquivo, int maxThreads) {
+
+	//for (int i = 1; i <= maxThreads; i++) {
+
+		int i =  maxThreads;
+
+		printf("------------------------------------ \n");
+		printf("Iniciando programa. Qtd de threads: %d\n", i);
+		clock_t tStart = clock();
+
+		executarFiltro(nomeArquivo, "Imagens/resultado.ppm", -100, i);
+
+		printf("Execucao encerrada.\n");
+		printf("Tempo total de execucao: %.2fs\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
+		printf("------------------------------------ \n");
+	//}
 }
 
 void executarFiltro(char* nomeArquivo, char* nomeArquivoResultado, int fBrilho, int qtdThreads) {
